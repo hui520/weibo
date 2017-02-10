c1388d7807b6f3b7a1cf72785ed2dbf27d75d948
workguy <18256278720.163.com>
2017-02-10 10:32:36 +0800

    创建了dog类

---
+++ weibo/weibo/Dog.h
@@ -0,0 +1,16 @@
+//
+//  Dog.h
+//  weibo
+//
+//  Created by workguy on 2017/2/10.
+//  Copyright © 2017年 workguy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+//
+@property (nonatomic,assign) int age;
+
+@end
---
+++ weibo/weibo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weibo
+//
+//  Created by workguy on 2017/2/10.
+//  Copyright © 2017年 workguy. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
